e3da63e874a7f0dd3bd331370faea3b4ecfb7dd4
DmitriyIL <Dmitriy.Lashchenov@waveaccess.ru>
2019-06-23 03:12:06 +0300

    parse file to ParsedMap

--- src/main/kotlin/data/Map.kt
+++ src/main/kotlin/data/Map.kt
@@ -2,17 +2,37 @@ package data
 
 data class Square(val x: Int, val y: Int)
 
-data class Obstacle(val listOfPoints: List<Square>)
+data class Booster(val square: Square, val type: BoosterType)
 
-data class Map(val vertices: List<Square>,
-               val left: Int,
-               val right: Int,
-               val top: Int,
-               val bottom: Int) {
+data class Obstacle(val squares: List<Square>)
+
+data class ParsedMap(
+    val vertices: List<Square>,
+    val start: Square,
+    val obstacles: List<Obstacle>,
+    val boosters: List<Booster>,
+    var maxX: Int = 0,
+    var maxY: Int = 0
+)
+
+data class Map(
+    val vertices: List<Square>,
+    val left: Int,
+    val right: Int,
+    val top: Int,
+    val bottom: Int
+) {
 
     val mapInMatrix = Array<IntArray>(top) { i -> (left..right).map { it -> it }.toIntArray() }
-    data class NotMarkedSquares(val listOfSquares: List<Square>)
 
+    data class NotMarkedSquares(val listOfSquares: List<Square>)
+}
+
+enum class BoosterType(str: String) {
+    MANIPULATOR("B"),
+    WHEELS("F"),
+    DRILL("L"),
+    UNKNOWN("X");
 }
 
 /**
