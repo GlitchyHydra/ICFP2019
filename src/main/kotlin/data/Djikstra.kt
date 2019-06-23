c5799236147b76e855b16b20fea0a9be88de8702
Valeriy <valeragta@inbox.ru>
2019-06-23 19:55:22 +0300

    djikstra bounds check

--- src/main/kotlin/data/Djikstra.kt
+++ src/main/kotlin/data/Djikstra.kt
@@ -11,12 +11,13 @@ class SquareInfo(
     }
 }
 
-fun Square.getNeighbors(): List<Square> {
+fun Square.getNeighbors(map: Array<IntArray>): List<Square> {
     val (x, y) = this
     val adj = mutableListOf<Square>()
     for (i in x - 1 .. x + 1) {
         for (j in y - 1 .. y + 1) {
-            if (x == i || y == j) {
+            if ((x == i || y == j) && this != Square(i, j)) {
+                if (j in 0..map.size && i in 0..map[j].size)
                 adj.add(Square(x, y))
             }
         }
@@ -39,7 +40,7 @@ fun shortestPath(from: Square, map: Array<IntArray>): Map<Square, SquareInfo> {
     while (queue.isNotEmpty()) {
         val currentInfo = queue.poll()
         val currentVertex = currentInfo.vertex
-        for (vertex in currentVertex.getNeighbors()) {
+        for (vertex in currentVertex.getNeighbors(map)) {
             val weight = 1
             if (weight != null) {
                 val newDistance = info[currentVertex]!!.distance + weight
